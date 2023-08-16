b31a7f3cb07100a26b03e5dc41d73391c639b5b1
sumeyyessmn <sumeyye.ssmn@gmail.com>
2023-08-16 22:22:25 +0300

    python file easy challenge completed

---
+++ Hafta-3/Easy.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Problem: Bir araba saatte 60 km hızla hareket ediyor. Bu araba kaç saatte 240 km yol alır?
+
+# In[17]:
+
+
+hız = 60 #km/saat
+yol = 240 #km
+
+
+
+# In[18]:
+
+
+saat = yol / hız #saat cinsinden hesaplanan süre 
+
+
+# In[19]:
+
+
+print(f"Araba {yol} km yol almak için {saat} kadar bir süre harcar.")
+
+
+# In[ ]:
+
+
+
+
