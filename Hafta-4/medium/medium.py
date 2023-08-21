5256f652915ebce3f7461c1ce20eb4c6966e0abc
sumeyyessmn <sumeyye.ssmn@gmail.com>
2023-08-21 19:34:14 +0300

    medium level python file completed

---
+++ Hafta-4/medium/medium.py
@@ -0,0 +1,11 @@
+## Problem : Bir dizi oluşturup bu sayıların ortalamasını hesaplayan bir kod parçacığı yazar mısın?
+
+myArray = [10,20,30,40]
+
+total = sum(myArray)
+
+length = len(myArray)
+
+average = total / length
+
+print(f"Dizideki sayıların ortalaması: {int(average)}")
