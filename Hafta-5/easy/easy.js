651735f72fbb4258f3893ed6a252d6526f8c8ce9
sumeyyessmn <sumeyye.ssmn@gmail.com>
2023-08-23 21:37:55 +0300

    hafta5 easy challenge completed

---
+++ Hafta-5/easy/easy.js
@@ -0,0 +1,11 @@
+//Problem: Bir dizi tanımladıktan sonra bu dizinin içinden en büyük sayıyı bulan kod parçacığını yazar mısın?
+let dizi = [1,2,3,4]
+let enBuyuk = dizi[0]
+
+  for(let i=0;i < dizi.length
+;i++){
+    if(dizi[i] > enBuyuk){
+      enBuyuk = dizi[i]
+    }
+  }
+alert(`Dizinin en büyük elemanı: ${enBuyuk}`)
