f75ef9567e201d5a940c9ce1d4ded6dbd8a825b6
sumeyyessmn <sumeyye.ssmn@gmail.com>
2023-08-26 19:36:14 +0300

    hafta 8 easy challenge completed

---
+++ Hafta-8/easy/easy.js
@@ -0,0 +1,5 @@
+// Problem: Easy: Kullanıcıdan alınan bir kelimenin uzunluğunu hesaplayan bir kod parçacığı yazar mısın?
+
+let word = prompt("Bir kelime giriniz: ")
+
+alert(`Kelimenin uzunluğu : ${word.length}`)
