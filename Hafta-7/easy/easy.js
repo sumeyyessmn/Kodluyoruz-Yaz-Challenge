4d1307e528790def524a4f9b03d2f3dbf40dfb3d
sumeyyessmn <sumeyye.ssmn@gmail.com>
2023-08-26 11:24:13 +0300

    hafta 7 easy challenge completed

---
+++ Hafta-7/easy/easy.js
@@ -0,0 +1,15 @@
+// Problem: Easy: Kullanıcıdan aldığın sayının karesini hesaplayarak ekrana yazdıran kod parçacığını yazar mısın?
+
+let number =  Number(prompt("Bir sayı giriniz: "))
+
+function kareAl(){
+    return number*number
+}
+
+let kare = kareAl()
+
+alert(`Girdiğiniz sayının karesi: ${kare} `)
+
+
+
+
