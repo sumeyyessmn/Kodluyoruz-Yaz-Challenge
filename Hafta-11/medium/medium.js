1529ea8ea1239bb938d8b3fdbe922bfb18a59663
sumeyyessmn <sumeyye.ssmn@gmail.com>
2023-08-30 13:45:24 +0300

    hafta 11 medium js completed

---
+++ Hafta-11/medium/medium.js
@@ -0,0 +1,8 @@
+// Problem:Bir öğrenci kitap okuma hedefi olarak yılda 36 kitap okumayı belirledi. Eğer her ay eşit sayıda kitap okursa kaç kitap okumuş olur?
+
+let hedef = 36
+let aySayisi = 12
+
+let kitapSayisi = hedef / aySayisi
+
+alert(`Her ay eşit sayıda kitap okursa her bir ayda ${kitapSayisi} tane kitap okur`)
